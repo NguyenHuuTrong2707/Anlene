2cc499e164a0dd3c33cda7cb00ea7a5f1d6e3639
NguyenHuuTrong2707 <nguyenhuutrongvtabs@gmail.com>
2025-01-08 10:51:35 +0700

    thực hiện kết nối firebase (demo đọc dữ liệu )

---
+++ app/firebase/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+// Cấu hình Firebase của bạn
+const firebaseConfig = {
+  apiKey: "AIzaSyBn3NVTvoAyLz0LfED6NTdZlVykirL1alc",
+  authDomain: "anlene-ef650.firebaseapp.com",
+  databaseURL: "https://anlene-ef650-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "anlene-ef650",
+  storageBucket: "anlene-ef650.firebasestorage.app",
+  messagingSenderId: "521349570357",
+  appId: "1:521349570357:web:6ea5268155445287a7367c",
+  measurementId: "G-V8TSY3YR8W"
+};
+
+// Khởi tạo Firebase
+export const app = initializeApp(firebaseConfig);
+
+
